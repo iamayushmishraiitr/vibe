ae27a2b6ec1d3637db05bc967a8c953b595e1fe3
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2024-08-26 01:11:20 +0530

    final

--- frontend/src/components/Poststats.tsx
+++ frontend/src/components/Poststats.tsx
@@ -1,5 +1,7 @@
 import { request } from "@/reqHandler";
 import { useState } from "react";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 const Poststats = (props: any) => {
   const data = props.info;
   const find = data.liked.find((item: any) =>  item === localStorage.getItem("userId") );
@@ -55,9 +57,9 @@ const Poststats = (props: any) => {
     <div className="flex  justify-between w-[100%] pl- pr-[1.3] mt-2">
       <div onClick={() => like()} className="flex">
         {likes ? (
-          <img src="../src/assets/liked.svg" />
+         <FavoriteIcon/>
         ) : (
-          <img src="../src/assets/like.svg" />
+           <FavoriteBorderIcon/>
         )}
         <h1 className="ml-0.5 text-xl mb-1">{number}</h1>
       </div>
