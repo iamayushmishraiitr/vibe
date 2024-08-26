720f71f0ee216f997ca52a2b8c980b3c7a701887
iamayushmishtaiitr <iamayushmishra1510@gmail.com>
2024-08-27 01:11:57 +0530

    final

--- frontend/src/reqHandler.ts
+++ frontend/src/reqHandler.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
-const baseUrl = "http://ec2-65-0-93-152.ap-south-1.compute.amazonaws.com:3000";
+const baseUrl = "https://vibe-azure.vercel.app";
 
+//http://ec2-65-0-93-152.ap-south-1.compute.amazonaws.com:3000
 const request= axios.create({
     baseURL:baseUrl ,
 })
